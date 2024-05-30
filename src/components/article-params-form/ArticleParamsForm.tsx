b0d428713574d5148aded5c3e1c7d201496c8f81
Polina Lepekhina <wdvrgnyjio6@gmail.com>
2024-05-30 19:26:18 +0300

    Прописана логика работы проекта

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,127 @@
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import clsx from 'clsx';
+import { useRef, useState } from 'react';
+import { Select } from '../select';
+import { Text } from '../text';
+import { Separator } from '../separator';
+import { RadioGroup } from '../radio-group';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+	OptionType,
+} from 'src/constants/articleProps';
+import { useOutsideClickClose } from '../select/hooks/useOutsideClickClose';
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	defaultOptions: ArticleStateType;
+	applyOptions: (newState: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	defaultOptions,
+	applyOptions,
+}: ArticleParamsFormProps) => {
+	const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+	const [selectedOptions, setSelectedOptions] =
+		useState<ArticleStateType>(defaultOptions);
+	const rootRef = useRef<HTMLDivElement | null>(null);
+
+	const toggleSideMenu = () => {
+		setIsSideMenuOpen(!isSideMenuOpen);
+	};
+
+	useOutsideClickClose({
+		isOpen: isSideMenuOpen,
+		rootRef,
+		onChange: setIsSideMenuOpen,
+	});
+
+	const handleSubmitSideMenu = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		applyOptions(selectedOptions);
+	};
+
+	const handleResetSideMenu = () => {
+		setSelectedOptions(defaultArticleState);
+		applyOptions(defaultArticleState);
+	};
+
+	function handleOptionsChange(
+		optionKey: keyof ArticleStateType,
+		value: OptionType
+	) {
+		setSelectedOptions((prevOption) => ({
+			...prevOption,
+			[optionKey]: value,
+		}));
+	}
+
 	return (
-		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton onClick={toggleSideMenu} isSideMenuOpen={isSideMenuOpen} />
+			<aside
+				className={clsx(styles.container, {
+					[styles.container_open]: isSideMenuOpen,
+				})}>
+				<form
+					className={styles.form}
+					onSubmit={handleSubmitSideMenu}
+					onReset={handleResetSideMenu}>
+					<Text weight={800} size={31} uppercase align='left'>
+						Задайте параметры
+					</Text>
+
+					<Select
+						title='шрифт'
+						options={fontFamilyOptions}
+						selected={selectedOptions.fontFamilyOption}
+						onChange={(value) => handleOptionsChange('fontFamilyOption', value)}
+					/>
+
+					<RadioGroup
+						title='размер шрифта'
+						name='размер шрифта'
+						options={fontSizeOptions}
+						selected={selectedOptions.fontSizeOption}
+						onChange={(value) => handleOptionsChange('fontSizeOption', value)}
+					/>
+
+					<Select
+						title='Цвет шрифта'
+						options={fontColors}
+						selected={selectedOptions.fontColor}
+						onChange={(value) => handleOptionsChange('fontColor', value)}
+					/>
+
+					<Separator />
+
+					<Select
+						title='Цвет фона'
+						options={backgroundColors}
+						selected={selectedOptions.backgroundColor}
+						onChange={(value) => handleOptionsChange('backgroundColor', value)}
+					/>
+
+					<Select
+						title='Ширина контента'
+						options={contentWidthArr}
+						selected={selectedOptions.contentWidth}
+						onChange={(value) => handleOptionsChange('contentWidth', value)}
+					/>
+
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
 						<Button title='Применить' type='submit' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	ArticleStateType,
+	defaultArticleState,
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +16,28 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [selectedOptions, setSelectedOptions] =
+		useState<ArticleStateType>(defaultArticleState);
+	const newArticleStateType = (data: ArticleStateType) => {
+		setSelectedOptions(data);
+	};
+
 	return (
 		<div
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': selectedOptions.fontFamilyOption.value,
+					'--font-size': selectedOptions.fontSizeOption.value,
+					'--font-color': selectedOptions.fontColor.value,
+					'--container-width': selectedOptions.contentWidth.value,
+					'--bg-color': selectedOptions.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				defaultOptions={defaultArticleState}
+				applyOptions={newArticleStateType}
+			/>
 			<Article />
 		</div>
 	);
